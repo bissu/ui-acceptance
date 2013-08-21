ae0ccd90d7e182432d56ac5d4570a68682ff9440
Marcelo Henrique <bissuh@gmail.com>
2013-08-21 01:51:59 -0300

    -n Initial commit

---
+++ scripts/eb4789d1.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("tshirtStoreApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("tshirtStoreApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
